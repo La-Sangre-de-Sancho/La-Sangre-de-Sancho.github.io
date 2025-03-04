5b28f186ad0147997718bb1bc05b7ab9264a9ffb
La-Sangre-de-Sancho <meng200648@126.com>
2025-03-04 21:44:12 +0800

    Site updated: 2025-03-04 21:44:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/04/Round-and-Round-We-Go/","2025/03/04/hello-world/"];function toRandomPost(){
+var posts=["2025/03/04/Round-and-Round-We-Go/","2025/03/04/hello-world/","2025/03/03/article-title/","2025/03/03/xiaoshiji1/","2025/03/03/xiaoshiji2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
