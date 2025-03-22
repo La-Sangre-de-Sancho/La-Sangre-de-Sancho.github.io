e8895cd6afff82a87e6dd115e52d5eeaae6b169a
La-Sangre-de-Sancho <meng200648@126.com>
2025-03-22 23:28:25 +0800

    Site updated: 2025-03-22 23:28:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/04/Round-and-Round-We-Go/","2025/03/04/hello-world/","2025/03/03/article-title/","2025/03/03/xiaoshiji1/","2025/03/03/xiaoshiji2/","2025/03/08/yibairihou/","2025/03/15/Psychological Statistics_2/"];function toRandomPost(){
+var posts=["2025/03/04/Round-and-Round-We-Go/","2025/03/04/hello-world/","2025/03/03/article-title/","2025/03/03/xiaoshiji1/","2025/03/03/xiaoshiji2/","2025/03/08/yibairihou/","2025/03/15/Psychological Statistics_2/","2025/03/22/xiaoshiji3/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
