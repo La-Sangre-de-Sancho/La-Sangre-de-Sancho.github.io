f4e2ed2b090f7c7b6ec987f6845d86a253d85d24
Ishmael <meng200648@gmail.com>
2026-07-01 21:23:15 +0800

    Site updated: 2026-07-01 21:23:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/09/Fungi/","2025/03/15/Psychological Statistics_2/","2025/04/08/dishijiuzhang/","2025/03/04/Round-and-Round-We-Go/","2025/03/03/article-title/","2025/04/20/canmou/","2026/06/28/hello-world/","2026/06/08/jiyideweilai/","2025/03/03/xiaoshiji1/","2025/03/03/xiaoshiji2/","2025/03/08/yibairihou/","2026/06/08/wumengdeshidai/","2025/03/22/xiaoshiji3/"];function toRandomPost(){
+var posts=["2026/03/09/Fungi/","2025/03/15/Psychological Statistics_2/","2025/04/08/dishijiuzhang/","2025/03/04/Round-and-Round-We-Go/","2025/03/03/article-title/","2025/04/20/canmou/","2026/06/28/hello-world/","2026/06/08/jiyideweilai/","2025/03/03/xiaoshiji1/","2025/03/03/xiaoshiji2/","2025/03/08/yibairihou/","2026/07/01/wumengdeshidai/","2025/03/22/xiaoshiji3/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
